05b70f84c9694d2ead7b9d447b2c03b2fd052315
Anders Kensby <anders.kensby@gmail.com>
2020-09-26 17:01:49 +0200

    add deleteTodo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,4 +1,4 @@
-import 'source-map-support/register'
+/*import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
@@ -9,3 +9,104 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   // TODO: Remove a TODO item by id
   return undefined
 }
+*/
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient = new XAWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Caller event', event)
+  const todoId = event.pathParameters.todoId
+
+  const result = await docClient.get({
+    TableName : todosTable,
+    Key: {
+      'id': todoId
+    }
+  }).promise()
+
+  if (result.Item) {
+    console.log("Item", result.Item)
+
+    const dresult = await docClient.delete({
+      TableName : todosTable,
+      Key: {
+        'id': todoId
+      }
+    }).promise()
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(dresult.Item)
+    }
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: 'Failed to delete. Item not found.'
+  }
+}
+
+/*
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient = new XAWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Caller event', event)
+  const todoId = event.pathParameters.todoId
+
+  const result = await docClient.get({
+    TableName : todosTable,
+    Key: {
+      HashKey: todoId
+    }
+  }).promise()
+
+  if (result.Count !== 0 && result.Item) {
+    console.log("Item", result.Item)
+
+    const dresult = await docClient.delete({
+      TableName : todosTable,
+      Key: {
+        HashKey: todoId
+      }
+    }).promise()
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(dresult.Item)
+    }
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: 'Failed to delete the item'
+  }
+}
+ */
